11e39742ff399ebbb69afd0fd078647c4e093cc1
Pasan Laksitha <29460924+Pasanlaksitha@users.noreply.github.com>
2023-11-20 21:55:40 +0530

    Update firebase-code.js

--- firebase-code.js
+++ firebase-code.js
@@ -8,14 +8,14 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDFVASLikevI6aElIlu7fTJkJRguC0Yzhk",
-    authDomain: "title-views-b2c2f.firebaseapp.com",
-    databaseURL: "https://title-views-b2c2f-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "title-views-b2c2f",
-    storageBucket: "title-views-b2c2f.appspot.com",
-    messagingSenderId: "191391074603",
-    appId: "1:191391074603:web:c8feaa13a7e81fe4175d6e",
-    measurementId: "G-WC5RZ7LF0N"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   // Initialize Firebase
